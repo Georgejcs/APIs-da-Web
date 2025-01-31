f7d4254d7fede96befb49cd247f5a9459bd653fb
Georgejcs <georgejos756@gmail.com>
2025-01-31 11:03:54 -0300

    catalogo

---
+++ APICatalogo/APICatalogo/Migrations/20250126210240_PopulaCategorias.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert into Categorias(Nome, ImagemUrl) Values('Bebidas', 'Bebidas.jpg')");
+            migrationBuilder.Sql("Insert into Categorias(Nome, ImagemUrl) Values('Lanches', 'Lanches.jpg')");
+            migrationBuilder.Sql("Insert into Categorias(Nome, ImagemUrl) Values('Sobremesas', 'Sobremesas.jpg')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Categorias");
+        }
+    }
+}
---
+++ APICatalogo/APICatalogo/Migrations/20250126211004_PopulaProdutos.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("insert into Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) Values('Coca Cola','Refrigerante', 9.90,'cocaCola.jpg', 30, now(), 1");
+            migrationBuilder.Sql("insert into Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) Values('x-bacon', 'Lanche de Bacon', 19.90, 'lancheBacon.jpg', 10, now(), 2");
+            migrationBuilder.Sql("insert into Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) Values('Milkshake', 'Milkshake 300ml', 15.90, 'milkshake.jpg', 100, now(), 3");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Produtos");
+        }
+    }
+}
